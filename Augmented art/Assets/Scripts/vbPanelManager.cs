ec255299ae08e9aaff752f7a817132df07604b90
Ivan <ivanaguilar01@live.com.mx>
2020-12-27 17:58:05 -0600

    Virtual button creation
    It was added a basic virtual button for showing the art information

---
+++ Augmented art/Assets/Scripts/vbPanelManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+public class vbPanelManager : MonoBehaviour
+{
+
+    public GameObject vbBtnObj;
+    public GameObject infoPlane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("BtnInformation");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+
+        infoPlane = GameObject.Find("Information");
+        infoPlane.SetActive(false);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button pressed");
+        infoPlane.SetActive(true);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button released");
+        infoPlane.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
