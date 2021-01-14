17ff062698baf8cde7f9465722f475a7fa0df00f
Ivan <ivanaguilar01@live.com.mx>
2021-01-14 16:29:38 -0600

    Home interface
    It was added the home interface with its UI elements, button functionality and animation

---
+++ Augmented art/Assets/Scripts/arCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arCamera : MonoBehaviour
+{
+    public GameObject cameraar;
+    void Start()
+    {
+        cameraar = GameObject.Find("ARCamera");
+        cameraar.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
