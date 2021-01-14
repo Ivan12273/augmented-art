f6136fa81eb17f010e47c5a3351c667e665163ce
Ivan <ivanaguilar01@live.com.mx>
2021-01-14 13:22:31 -0600

    Screenshot button
    It was added the screenshot buttom

---
+++ Augmented art/Assets/Scripts/btnScreenshot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnScreenshot : MonoBehaviour
+{
+    public void Capture()
+    {
+        NativeToolkit.SaveScreenshot("Screenshot-AA", "Screenshots", "png");
+    }
+}
